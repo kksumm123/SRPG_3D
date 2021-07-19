cf783a9067c3a6be51ecb3ab39de9099c1785bc4
kksumm123 <kksumm123@gmail.com>
2021-07-19 17:51:14 +0900

    초기 세팅

---
+++ Assets/GroundManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundManager : MonoBehaviour
+{
+    [SerializeField] Vector2Int playerPos; // 플레이어 위치
+    [SerializeField] Vector2Int goalPos;   // 클릭한 위치 (이동목표)
+    [SerializeField] Dictionary<Vector2Int, int> map; // 블록 맵 지정하기
+    [SerializeField] List<int> passableValues; // TilType을 int로 받기
+    enum TileType
+    {
+        Walkable, // 갈 수 있는 지역
+        Wall, // 갈 수 없는 지역
+    }
+    void Start()
+    {
+        passableValues = new List<int>();
+        passableValues.Add((int)TileType.Walkable);
+
+        var path = PathFinding2D.find4(playerPos, goalPos, map, passableValues);
+    }
+
+}
