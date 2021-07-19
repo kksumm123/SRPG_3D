a9769ac5d0b4105e93b98e243fc00f9599ca3438
kksumm123 <kksumm123@gmail.com>
2021-07-19 17:53:27 +0900

    BlockInfo.cs 작성완료

--- Assets/BlockInfo.cs
+++ Assets/BlockInfo.cs
@@ -8,15 +8,5 @@ public enum BlockType
 }
 public class BlockInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public BlockType blockType;
 }
