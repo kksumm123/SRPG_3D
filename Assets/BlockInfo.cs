d6704a757e5501c1a2a180ff79601c4e89e5e64f
kksumm123 <kksumm123@gmail.com>
2021-07-19 20:41:42 +0900

    BlockInfo.cs 수정 PanAndZoom 추가에 따른 캐릭터 이동 좌표 얻는 로직 수정 PanAndZoom 기능 = 드래그해서 화면이동

--- Assets/BlockInfo.cs
+++ Assets/BlockInfo.cs
@@ -9,9 +9,19 @@ public enum BlockType
 public class BlockInfo : MonoBehaviour
 {
     public BlockType blockType;
-
+    Vector3 mouseDownPosition;
+    public float clickDistance = 1;
     private void OnMouseDown()
     {
+        mouseDownPosition = Input.mousePosition;
+    }
+    private void OnMouseUp()
+    {
+        // 다운 - 업 거리가 특정 거리보다 크면 리턴
+        var mouseUpPosition = Input.mousePosition;
+        if (Vector3.Distance(mouseDownPosition, mouseUpPosition) > clickDistance)
+            return;
+        // 작으면 캐릭터 이동
         GroundManager.Instance.OnTouch(transform.position);
     }
 }
