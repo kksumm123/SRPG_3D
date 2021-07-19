c46d1728e99d5da50169edf91eb084d70f777aa2
kksumm123 <kksumm123@gmail.com>
2021-07-19 20:17:43 +0900

    FollowTarget.cs 작성완료

--- Assets/FollowTarget.cs
+++ Assets/FollowTarget.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowTarget : MonoBehaviour
+public class FollowTarget : SingletonMonoBehavior<FollowTarget>
 {
-    
+    Transform target;
+    [SerializeField] Vector3 offset;
+
+    public void SetTarget(Transform _target)
+    {
+        this.target = _target;
+    }
+
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        var newPow = target.position + offset;
+        newPow.y = transform.position.y;
+        transform.position = newPow;
+    }
 }
