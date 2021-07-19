51200ddee7c5f642f26a1dea0861bae4c77f699f
kksumm123 <kksumm123@gmail.com>
2021-07-19 17:52:53 +0900

    1. 모든 블록들에 BlockInfo.cs 추가 2. BlockType추가 3. TileType을 BlockType으로 대체

--- Assets/GroundManager.cs
+++ Assets/GroundManager.cs
@@ -8,15 +8,15 @@ public class GroundManager : MonoBehaviour
     [SerializeField] Vector2Int goalPos;   // 클릭한 위치 (이동목표)
     [SerializeField] Dictionary<Vector2Int, int> map; // 블록 맵 지정하기
     [SerializeField] List<int> passableValues; // TilType을 int로 받기
-    enum TileType
-    {
-        Walkable, // 갈 수 있는 지역
-        Wall, // 갈 수 없는 지역
-    }
+    //enum TileType
+    //{
+    //    Walkable, // 갈 수 있는 지역
+    //    Wall, // 갈 수 없는 지역
+    //}
     void Start()
     {
         passableValues = new List<int>();
-        passableValues.Add((int)TileType.Walkable);
+        passableValues.Add((int)BlockType.Walkable);
 
         var path = PathFinding2D.find4(playerPos, goalPos, map, passableValues);
     }
