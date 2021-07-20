e2770b0006faaabf3b8a46744217e2e3f9c9c3b7
kksumm123 <kksumm123@gmail.com>
2021-07-20 16:51:18 +0900

    A*알고리즘 버그 수정 -11, 0, -13 -> -10, 0, -14 위치로 이동할 때 길 이상하게 가던거

--- Assets/PathFinding2D.cs
+++ Assets/PathFinding2D.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 using System;
 using System.Linq;
@@ -121,12 +121,12 @@ public static class PathFinding2D
 
             if (temp == null) return 1;
 
-            if (Mathf.Abs(this.fScore - temp.fScore) < 0.01f)
+            if (Mathf.Abs(this.fScore - temp.fScore) > 0.01f)
             {
                 return this.fScore > temp.fScore ? 1 : -1;
             }
 
-            if (Mathf.Abs(this.hScore - temp.hScore) < 0.01f)
+            if (Mathf.Abs(this.hScore - temp.hScore) > 0.01f)
             {
                 return this.hScore > temp.hScore ? 1 : -1;
             }
