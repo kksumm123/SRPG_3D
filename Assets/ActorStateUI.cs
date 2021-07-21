9373ef8ac2cc3bc409101a7267e1a19fbe437fca
kksumm123 <kksumm123@gmail.com>
2021-07-21 20:43:47 +0900

    HP, MP 게이지 연동

--- Assets/ActorStateUI.cs
+++ Assets/ActorStateUI.cs
@@ -7,12 +7,39 @@ public class ActorStateUI : SingletonMonoBehavior<ActorStateUI>
 {
     Text nickName;
     Text status;
+    RectTransform hpBarGauge;
+    RectTransform hpBar;
+    RectTransform mpBarGauge;
+    RectTransform mpBar;
+    Image hpBarGaugeImage;
+    Image mpBarGaugeImage;
+
 
     public void Show(Actor actor)
     {
         base.Show();
         nickName = transform.Find("Name").GetComponent<Text>();
         status = transform.Find("State").GetComponent<Text>();
+
+        hpBarGauge = transform.Find("HPBar/Gauge").GetComponent<RectTransform>();
+        hpBar = transform.Find("HPBar/BG").GetComponent<RectTransform>();
+        mpBarGauge = transform.Find("MPBar/Gauge").GetComponent<RectTransform>();
+        mpBar = transform.Find("MPBar/BG").GetComponent<RectTransform>();
+        hpBarGaugeImage = hpBarGauge.GetComponent<Image>();
+        mpBarGaugeImage = mpBarGauge.GetComponent<Image>();
+
+        var size = hpBarGauge.sizeDelta;
+        size.x = actor.maxHP;
+        hpBarGauge.sizeDelta = size;
+        hpBar.sizeDelta = size;
+
+        size = mpBarGauge.sizeDelta;
+        size.x = actor.maxMP;
+        mpBarGauge.sizeDelta = size;
+        mpBar.sizeDelta = size;
+
+        hpBarGaugeImage.fillAmount = actor.hp / actor.maxHP;
+        mpBarGaugeImage.fillAmount = actor.mp / actor.maxMP;
 
         nickName.text = actor.nickName;
         status.text = actor.status.ToString();
