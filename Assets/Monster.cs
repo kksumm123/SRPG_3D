fc214036aaf12f0d60aef41fe30c219206296199
kksumm123 <kksumm123@gmail.com>
2021-07-21 20:16:15 +0900

    1. Monster.cs 내에 Class Actor생성 2. Monster, Player 가 Actor 상속

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Monster : MonoBehaviour
+public enum StatusType
+{
+    Normal,
+    Sleep,
+    Die,
+}
+public class Actor : MonoBehaviour
+{
+    public string nickName;
+    public float hp;
+    public float mp;
+    public StatusType status;
+}
+public class Monster : Actor
 {
     Animator animator;
+
     void Start()
     {
         GroundManager.Instance.AddBlockInfo(transform.position, BlockType.Monster);
