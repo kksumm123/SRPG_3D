d5e7c6d84e319b5c3a361dcff87a94cad059ca25
kksumm123 <kksumm123@gmail.com>
2021-07-22 16:07:08 +0900

    1. 불필요한 코드 리팩토링 2. 3DTilemap 수업을 위해 2DTilemap 패키지 설치

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,15 +7,14 @@ public class Player : Actor
 {
     public static Player SelectedPlayer;
     Animator animator;
-    Transform player;
     [SerializeField] float rotatelerpValue = 0.05f;
     [SerializeField] float moveDelay = 0.3f;
     void Start()
     {
         SelectedPlayer = this;
         animator = GetComponentInChildren<Animator>();
-        player = transform;
         GroundManager.Instance.AddBlockInfo(transform.position, BlockType.Player, this);
+        FollowTarget.Instance.SetTarget(transform);
     }
     public void PlayAnimation(string stateName)
     {
@@ -31,7 +30,7 @@ public class Player : Actor
     [SerializeField] BlockType passableValues = BlockType.Walkable | BlockType.Water;
     IEnumerator FindPathCo(Vector2Int goalPos)
     {
-        Vector2Int playerPos = player.position.ToVector2Int();
+        Vector2Int playerPos = transform.position.ToVector2Int();
         var map = GroundManager.Instance.map;
         var path = PathFinding2D.find4(playerPos, goalPos, map, passableValues);
         if (path.Count == 0)
@@ -39,22 +38,24 @@ public class Player : Actor
         else
         {
             // 원래 위치에서 플레이어 정보 삭제
-            GroundManager.Instance.RemoveBlockInfo(Player.SelectedPlayer.transform.position, BlockType.Player);
-            Player.SelectedPlayer.PlayAnimation("Run");
-            FollowTarget.Instance.SetTarget(Player.SelectedPlayer.transform);
+            GroundManager.Instance
+                .RemoveBlockInfo(transform.position, BlockType.Player);
+            PlayAnimation("Run");
+            FollowTarget.Instance.SetTarget(transform);
             path.RemoveAt(0);
             foreach (var item in path)
             {
                 Vector3 playerNewPos = new Vector3(item.x, 0, item.y);
                 StartCoroutine(PlayerLookAtLerp(playerNewPos));
-                player.DOMove(playerNewPos, moveDelay).SetEase(Ease.Linear);
+                transform.DOMove(playerNewPos, moveDelay).SetEase(Ease.Linear);
                 yield return new WaitForSeconds(moveDelay);
             }
         }
         Player.SelectedPlayer.PlayAnimation("Idle");
         FollowTarget.Instance.SetTarget(null);
         // 이동한 위치에 플레이어 정보 추가 
-        GroundManager.Instance.AddBlockInfo(Player.SelectedPlayer.transform.position, BlockType.Player, this);
+        GroundManager.Instance
+            .AddBlockInfo(transform.position, BlockType.Player, this);
     }
 
     internal bool OnMoveable(Vector3 position)
@@ -78,8 +79,8 @@ public class Player : Actor
         var endTime = Time.time + moveDelay;
         while (endTime > Time.time)
         {
-            player.forward = Vector3.Slerp(player.forward
-                    , (playerNewPos - player.position).normalized, rotatelerpValue);
+            transform.forward = Vector3.Slerp(transform.forward
+                    , (playerNewPos - transform.position).normalized, rotatelerpValue);
             yield return null;
         }
     }
