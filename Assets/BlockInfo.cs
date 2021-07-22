eef6bc9486037de02cebe3fb24c81509d9494a9f
kksumm123 <kksumm123@gmail.com>
2021-07-22 20:02:58 +0900

    색칠된 영역 되돌리는 코드 추가

--- Assets/BlockInfo.cs
+++ Assets/BlockInfo.cs
@@ -15,6 +15,8 @@ public enum BlockType
 }
 public class BlockInfo : MonoBehaviour
 {
+    public static List<BlockInfo> movableBlocks = new List<BlockInfo>();
+
     Renderer m_Renderer;
     Color m_MouseOverColor = Color.red;
     Color m_OriginalColor;
@@ -26,6 +28,7 @@ public class BlockInfo : MonoBehaviour
     public BlockType blockType;
     Vector3 mouseDownPosition;
     public float clickDistance = 1;
+    bool isChangedColor = false;
     private void OnMouseDown()
     {
         mouseDownPosition = Input.mousePosition;
@@ -37,17 +40,29 @@ public class BlockInfo : MonoBehaviour
         // 작으면 캐릭터 이동
         if (Vector3.Distance(mouseDownPosition, mouseUpPosition) > clickDistance)
             return;
+
+        // 이미 빨간 블럭 상태일 때 다시 선택하면 빨간 블럭 원상 복구
+        if (movableBlocks.Count > 0)
+        {
+            foreach (var item in movableBlocks)
+                item.ToChangeOriginColor();
+            movableBlocks.Clear();
+        }
+
         if (actor && actor == Player.SelectedPlayer)
         { // 클릭한 위치에 actor가 있고, 플레이어가 있다면
-            //영역표시
-            // 첫번째 이동으로 갈 수 있는 곳을 추가
+          //영역표시
+          // 첫번째 이동으로 갈 수 있는 곳을 추가
             ShowMoveDistance(actor.moveDistance);
         }
-        Player.SelectedPlayer.OnTouch(transform.position);
-    }
+        // 지금 블럭에 몬스터 있으면 때리기
+        if (transform.position != Player.SelectedPlayer.transform.position)
+            Player.SelectedPlayer.OnTouch(transform.position);
 
+    }
     void ShowMoveDistance(int moveDistance)
     {
+        movableBlocks.Clear();
         //var blocks = Physics.OverlapSphere(transform.position, actor.moveDistance);
         var rotate = Quaternion.Euler(0, 45, 0);
         Vector3 halfExtents = (moveDistance / Mathf.Sqrt(2)) * 0.99f * Vector3.one;
@@ -55,7 +70,14 @@ public class BlockInfo : MonoBehaviour
         foreach (var item in blocks)
         {
             if (Player.SelectedPlayer.OnMoveable(item.transform.position, moveDistance))
-                item.GetComponent<BlockInfo>()?.ToChangeRedColor();
+            {
+                var itemBlocks = item.GetComponent<BlockInfo>();
+                if (itemBlocks != null)
+                {
+                    itemBlocks.ToChangeRedColor();
+                    movableBlocks.Add(itemBlocks);
+                }
+            }
         }
     }
 
@@ -79,7 +101,7 @@ public class BlockInfo : MonoBehaviour
 
         GetComponentInChildren<TextMesh>().text = debugText.ToString();
     }
-    void ContaingText(StringBuilder sb, BlockType walkable)
+    public void ContaingText(StringBuilder sb, BlockType walkable)
     {
         if (blockType.HasFlag(walkable))
             sb.AppendLine(walkable.ToString());
@@ -95,6 +117,10 @@ public class BlockInfo : MonoBehaviour
     public void ToChangeRedColor()
     {
         m_Renderer.material.color = m_MouseOverColor;
+    }
+    void ToChangeOriginColor()
+    {
+        m_Renderer.material.color = m_OriginalColor;
     }
 
     private void OnMouseExit()
