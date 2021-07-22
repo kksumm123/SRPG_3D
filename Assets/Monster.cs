3c6d2dbddc06355f5f442f2f85d605d76ca31b10
kksumm123 <kksumm123@gmail.com>
2021-07-22 20:35:21 +0900

    Actor 클래스에 공격범위 리스트 추가

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -18,6 +18,18 @@ public class Actor : MonoBehaviour
     public StatusType status;
 
     public int moveDistance = 5;
+
+    // 공격 범위를 모아두자
+    public List<Vector2Int> attackablePoints = new List<Vector2Int>();
+    private void Awake()
+    {
+        var attackPoints = GetComponents<AttackPoint>();
+
+        foreach (var item in attackPoints)
+            attackablePoints.Add(item.transform.localPosition.ToVector2Int());
+
+        // 
+    }
 }
 public class Monster : Actor
 {
