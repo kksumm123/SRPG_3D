a0508caa5c7bab793edc8f3ded90c5493265c93b
kksumm123 <kksumm123@gmail.com>
2021-07-28 16:19:08 +0900

    SaveInt에서 에디터 관련 내용 분리 *Editor스크립트 관련 내용은 컴파일할 때 오래걸린다 따라서 따로 Editor 폴더에 분할하여 관리하는 것이 좋다

--- Assets/Scripts/Common/SaveInt.cs
+++ Assets/Scripts/Common/SaveInt.cs
@@ -1,39 +1,6 @@
 ﻿using System;
 using UnityEngine;
 
-
-#if UNITY_EDITOR
-using UnityEditor;
-[CustomPropertyDrawer(typeof(SaveInt))]
-public class SaveIntPropertyDrawer : PropertyDrawer
-{
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        EditorGUI.BeginProperty(position, label, property);
-
-        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-        var left = position; left.xMax -= 40;
-        var right = position; right.xMin = left.xMax + 2;
-
-        var value = property.FindPropertyRelative("value");
-        EditorGUI.PropertyField(left, value, GUIContent.none);
-
-        if (GUI.Button(right, "Save"))
-        {
-            var key = property.FindPropertyRelative("key").stringValue;
-            if (string.IsNullOrEmpty(key) == false)
-            {
-                PlayerPrefs.SetInt(key, value.intValue);
-                PlayerPrefs.Save();
-            }
-        }
-
-        EditorGUI.EndProperty();
-    }
-}
-#endif
-
 [Serializable]
 public class SaveInt 
 {
