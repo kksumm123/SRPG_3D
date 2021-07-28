33ceb449f30e23fc89cf81540d8f9c80905dda81
kksumm123 <kksumm123@gmail.com>
2021-07-28 17:21:12 +0900

    Actor.OnDestroy() 오류 해결  - 플레이 종료될때는 스크립트들이 하나씩 Destroy된다, 따라서 무엇이 먼저 종료되는지에 따라 오류가 날 수도

--- Assets/Actor.cs
+++ Assets/Actor.cs
@@ -64,7 +64,7 @@ public class Actor : MonoBehaviour
     }
     protected void OnDestroy()
     { // 죽을 때, 밟고있는 블럭에 대한 정보 삭제
-        GroundManager.Instance.RemoveBlockInfo(transform.position, GetBlockType());
+        GroundManager.Instance?.RemoveBlockInfo(transform.position, GetBlockType());
     }
     float takeHitTime = 0.7f;
     public IEnumerator TakeHitCo(int power)
