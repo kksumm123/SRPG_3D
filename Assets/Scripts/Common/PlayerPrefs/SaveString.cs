376cf231168b9efcf8f19ea2cb21a135a4622e78
kksumm123 <kksumm123@gmail.com>
2021-07-28 16:14:13 +0900

    SaveString 다른 파일로 교체

--- Assets/Scripts/Common/PlayerPrefs/SaveString.cs
+++ Assets/Scripts/Common/PlayerPrefs/SaveString.cs
@@ -1,25 +1,62 @@
 ﻿using UnityEngine;
+using System;
 
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(SaveString))]
+public class SaveStringPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        var left = position; left.xMax -= 40;
+        var right = position; right.xMin = left.xMax + 2;
+
+        var value = property.FindPropertyRelative("value");
+        EditorGUI.PropertyField(left, value, GUIContent.none);
+
+        if (GUI.Button(right, "Save"))
+        {
+            var key = property.FindPropertyRelative("key").stringValue;
+            if (string.IsNullOrEmpty(key) == false)
+            {
+                PlayerPrefs.SetString(key, value.stringValue);
+                PlayerPrefs.Save();
+            }
+        }
+
+        EditorGUI.EndProperty();
+    }
+}
+#endif
+
+[Serializable]
 public class SaveString
 {
-    readonly string key;
+    [SerializeField] string key;
+
+    [SerializeField]
     string value;
-    public SaveString(string key)
+    public SaveString(string _key)
     {
-        this.key = Application.dataPath + key;
+        //key = Application.dataPath + GetType() + _key;
+        key = GetType() + _key;
+
+        if (PlayerPrefs.HasKey(key))
+            value = PlayerPrefs.GetString(key);
+        else
+            value = "";
     }
 
     public string Value
     {
         get
         {
-            if (value == null)
-            {
-                if (PlayerPrefs.HasKey(key))
-                    value = PlayerPrefs.GetString(key);
-                else
-                    value = "";
-            }
             return value;
         }
         set
