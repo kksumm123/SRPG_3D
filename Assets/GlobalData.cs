7db0a81ba3ceb6de492e217aee52e3e70d3af8d6
kksumm123 <kksumm123@gmail.com>
2021-07-28 20:10:33 +0900

    Class ItemData, ItemDropInfo, DropItemGroupData 추가

--- Assets/GlobalData.cs
+++ Assets/GlobalData.cs
@@ -10,13 +10,42 @@ public class PlayerLevelData
     public int maxMp;
     public int maxExp;
 }
+[System.Serializable]
+public class ItemData
+{
+    public int ID;
+    public int allowLevel;
+    public int sellPrice;
+    public int buyPrice;
+    public string iconName;
+}
+[System.Serializable]
+public class ItemDropInfo
+{
+    public int dropItemID;
+    public float ratio; // 드랍확률
+}
+[System.Serializable]
+public class DropItemGroupData
+{
+    public int ID;
+    public List<ItemDropInfo> dropItems;
+}
 
 public class GlobalData : SingletonMonoBehavior<GlobalData>
 {
-    [SerializeField] List<PlayerLevelData> playerDatas;
+    [SerializeField] List<PlayerLevelData> playerDatas = new List<PlayerLevelData>();
     public Dictionary<int, PlayerLevelData> playerDataMap;
+
+    [SerializeField] List<ItemData> itemDatas = new List<ItemData>();
+    public Dictionary<int, ItemData> itemDataMap;
+
+    [SerializeField] List<DropItemGroupData> dropItemGroupData = new List<DropItemGroupData>();
+    public Dictionary<int, DropItemGroupData> dropItemGroupDataMap;
     protected override void OnInit()
     {
         playerDataMap = playerDatas.ToDictionary(x => x.level);
+        itemDataMap = itemDatas.ToDictionary(x => x.ID);
+        dropItemGroupDataMap = dropItemGroupData.ToDictionary(x => x.ID);
     }
 }
