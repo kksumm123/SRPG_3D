48310b79b3b5df33c3a09e1abf7f49519118ee84
kksumm123 <kksumm123@gmail.com>
2021-07-28 16:41:16 +0900

    1. GlobalData.cs 초안 작성완료, 2. GlobalData오브젝트에 레벨 3까지의 정보 입력

--- Assets/GlobalData.cs
+++ Assets/GlobalData.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+public class PlayerLevelData
+{
+    public int level;
+    public int hp;
+    public int mp;
+    public int maxExp;
+}
 
 public class GlobalData : SingletonMonoBehavior<GlobalData>
 {
-    
+    public List<PlayerLevelData> playerDatas;
 }
